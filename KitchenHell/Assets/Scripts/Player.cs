8505a91d1e94434f4223f271d0cf246003d25633
SBola97 <sebassenk1997@gmail.com>
2024-07-16 01:10:05 +0200

    Added logic for highlight clear counter selected (visual) using Singleton pattern

--- KitchenHell/Assets/Scripts/Player.cs
+++ KitchenHell/Assets/Scripts/Player.cs
@@ -8,32 +8,33 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public static Player Instance { get; private set; }
+    public event EventHandler<OnSelectedCounterChangedEventArgs> OnSelectedCounterChanged;
+    public class OnSelectedCounterChangedEventArgs : EventArgs{
+        public ClearCounter selectedCounter;
+    }
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private GameInput gameInput;
     [SerializeField] private LayerMask countersLayerMask;
     private bool isWalking;
     private Vector3 lastInteractDir;
+    private ClearCounter selectedCounter;
+
+    private void Awake() {
+        if(Instance != null){
+            Debug.LogError("There is more than 1 Player instance");
+        }
+        Instance = this;
+    }
 
     // Start is called before the first frame update
     private void Start(){
         gameInput.OnInteractAction += GameInput_OnInteraction;
-
     }
 
-    private void GameInput_OnInteraction(object sender, EventArgs e)
-    {
-        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
-        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-
-        if(moveDir != Vector3.zero){
-            lastInteractDir = moveDir;
-        }
-
-        float interactDistance = 2f;
-        if(Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, countersLayerMask)){
-            if(raycastHit.transform.TryGetComponent(out ClearCounter clearCounter)){
-                clearCounter.Interact();
-            }
+    private void GameInput_OnInteraction(object sender, EventArgs e){
+        if(selectedCounter != null){
+            selectedCounter.Interact();
         }
     }
 
@@ -55,11 +56,19 @@ public class Player : MonoBehaviour
         float interactDistance = 2f;
         if(Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, countersLayerMask)){
             if(raycastHit.transform.TryGetComponent(out ClearCounter clearCounter)){
-
+                //Has ClearCounter
+                if(clearCounter != selectedCounter){
+                    SetSelectedCounter(clearCounter);
+                }
+            }
+            else{
+                SetSelectedCounter(null);   
             }
         }
+        else{
+            SetSelectedCounter(null);
+        }
     }
-
     private void HandleMovement(){
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
@@ -126,5 +135,13 @@ public class Player : MonoBehaviour
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
         //Debug.Log(inputVector);
+    }
+
+    private void SetSelectedCounter(ClearCounter selectedCounter){
+        this.selectedCounter = selectedCounter;
+
+        OnSelectedCounterChanged?.Invoke(this, new OnSelectedCounterChangedEventArgs{
+            selectedCounter = selectedCounter   
+        });
     }
 }
