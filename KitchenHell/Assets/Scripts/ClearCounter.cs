0e77411fdf9623e278605f6465de66a9a327b56b
SBola97 <sebassenk1997@gmail.com>
2024-10-29 00:25:48 +0100

    Added logic to drop kitchen objects on the clear counters and pick them up by the player

--- KitchenHell/Assets/Scripts/ClearCounter.cs
+++ KitchenHell/Assets/Scripts/ClearCounter.cs
@@ -7,5 +7,29 @@ public class ClearCounter : BaseCounter
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
 
     public override void Interact(Player player){
+        //There is no kitchenObject here
+        if(!HasKitchenObject()){
+            //Player is carrying a kitchenObject
+            if(player.HasKitchenObject()){
+                //Drop it to the counter
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            }
+            else{
+                //Player is not carrying anything
+            }
+            
+
+        }
+        //There is a kitchenObject here
+        else{
+            if(player.HasKitchenObject()){
+                //Player's carrying something
+            }
+            //Player isn't carrying anything
+            else{
+                //Give it to the player
+                GetKitchenObject().SetKitchenObjectParent(player);
+            }
+        }
     }
 }
--- KitchenHell/Assets/Scripts/ContainerCounter.cs
+++ KitchenHell/Assets/Scripts/ContainerCounter.cs
@@ -9,10 +9,13 @@ public class ContainerCounter : BaseCounter
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
 
     public override void Interact(Player player){
+        //Checks if player is carrying something already
+        if(!player.HasKitchenObject()){
             //Spawns the object and gives it to the Player
             Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab); 
             kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
             //Fires off the event
             OnPlayerGrabbedObject?.Invoke(this, EventArgs.Empty);
+        }
     }
 }
