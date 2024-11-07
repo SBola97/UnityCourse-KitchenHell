62344bf85f27e72d68835144b9a1cc91b9a8062d
SBola97 <sebassenk1997@gmail.com>
2024-11-07 01:33:28 +0100

    Added prefabs for sliced KitchenObjects and implemented recipes with Scriptable Objects to control if a KO can be cut

--- KitchenHell/Assets/Scripts/CuttingCounter.cs
+++ KitchenHell/Assets/Scripts/CuttingCounter.cs
@@ -1,25 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CuttingCounter : BaseCounter
 {
-    [SerializeField] private KitchenObjectSO cutkitchenObjectSO;
+    [SerializeField] private CuttingRecipeSO[] cuttingRecipeSOArray;
     public override void Interact(Player player){
-        //There is no kitchenObject here
+        //There is no KitchenObject here
         if(!HasKitchenObject()){
-            //Player is carrying a kitchenObject
+            //Checks if player is carrying a KitchenObject
             if(player.HasKitchenObject()){
-                //Drop it to the counter
-                player.GetKitchenObject().SetKitchenObjectParent(this);
+                //Checks if the KitchenObject belongs to a recipe
+                if(HasRecipeWithInput(player.GetKitchenObject().GetKitchenObjectSO())){
+                    //Drop it on the counter to cut it
+                    player.GetKitchenObject().SetKitchenObjectParent(this);
+                }
             }
             else{
-                //Player is not carrying anything
+                //Player is not carrying anything or KitchenObject can't be cut
             }
-            
-
         }
-        //There is a kitchenObject here
+        //There is a KitchenObject here
         else{
             if(player.HasKitchenObject()){
                 //Player's carrying something
@@ -32,12 +34,32 @@ public class CuttingCounter : BaseCounter
         }
     }
 
-    public override void InteractAlternate(Player player)
-    {
-        //Check if there is something on top of it
-        if(HasKitchenObject()){
+    public override void InteractAlternate(Player player){
+        //Check if there is a KitchenObject here AND if it can be cut
+        if(HasKitchenObject() && HasRecipeWithInput(GetKitchenObject().GetKitchenObjectSO())){
+            KitchenObjectSO outputKitchenObjectSO = getOutputForInput(GetKitchenObject().GetKitchenObjectSO());
+
             GetKitchenObject().DestroySelf();
-            KitchenObject.SpawnKitchenObject(cutkitchenObjectSO, this);
+
+            KitchenObject.SpawnKitchenObject(outputKitchenObjectSO, this);
         }
+    }
+
+    private bool HasRecipeWithInput(KitchenObjectSO kitchenObjectSO){
+        foreach (CuttingRecipeSO cuttingRecipeSO in cuttingRecipeSOArray){
+            if(cuttingRecipeSO.input == kitchenObjectSO){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private KitchenObjectSO getOutputForInput(KitchenObjectSO inputKitchenObjectSO){
+        foreach (CuttingRecipeSO cuttingRecipeSO in cuttingRecipeSOArray){
+            if(cuttingRecipeSO.input == inputKitchenObjectSO){
+                return cuttingRecipeSO.output;
+            }
+        }
+        return null;
     }
 }
